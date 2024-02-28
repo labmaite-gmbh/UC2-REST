4a61f01f3ed123b409c6ccf0a5989614d349a1be
matias <matiasgstingl@gmail.com>
2024-02-28 12:12:20 +0100

    Tested UC2I in Worker5 -> copied uc2rest from laptop.

--- uc2rest/UC2Client.py
+++ uc2rest/UC2Client.py
@@ -7,14 +7,21 @@ Copyright 2021 Benedict Diederich, released under LGPL 3.0 or later
 """
 from .mserial import Serial
 from .mserial import SerialManagerWrapper
-
-try:
-    from .imswitch.imcommon.model import initLogger
-    IS_IMSWITCH = True
-except:
-    print("No imswitch available")
-    from .logger import Logger
-    IS_IMSWITCH = False
+from .galvo import Galvo
+from .config import config
+from .ledmatrix import LedMatrix
+from .motor import Motor
+from .home import Home
+from .state import State
+from .laser import Laser
+from .wifi import Wifi
+from .camera import Camera
+from .analog import Analog
+from .modules import Modules
+from .digitalout import DigitalOut
+from .rotator import Rotator
+from .logger import Logger
+from .cmdrecorder import cmdRecorder
 
 try:
     import requests
@@ -33,7 +40,7 @@ class UC2Client(object):
     # BAUDRATE = 500000
     BAUDRATE = 115200
 
-    def __init__(self, host=None, port=31950, serialport=None, identity="UC2_Feather", baudrate=BAUDRATE, NLeds=64, SerialManager=None, DEBUG=False):
+    def __init__(self, host=None, port=31950, serialport=None, identity="UC2_Feather", baudrate=BAUDRATE, NLeds=64, SerialManager=None, DEBUG=False, logger=None):
         '''
         This client connects to the UC2-REST microcontroller that can be found here
         https://github.com/openUC2/UC2-REST
@@ -45,14 +52,12 @@ class UC2Client(object):
 
         you can send commands through wifi/http or usb/serial
         '''
-
-        if IS_IMSWITCH:
-            self.logger = initLogger(self, tryInheritParent=True)
-        else:
-            from .logger import Logger
+        if logger is None:
             self.logger = Logger()
-        # set default APIVersion
-        self.APIVersion = 2
+        else:
+            self.logger = logger
+
+        # perhaps we are in the browser?
         self.isPyScript = False
 
         # initialize communication channel (# connect to wifi or usb)
@@ -60,7 +65,7 @@ class UC2Client(object):
             # use USB connection
             self.serial = Serial(serialport, baudrate, parent=self, identity=identity, DEBUG=DEBUG)
             self.is_serial = True
-            self.is_connected = True
+            self.is_connected = self.serial.is_connected
             self.serial.DEBUG = DEBUG
         elif host is not None:
             # use client in wireless mode
@@ -78,35 +83,11 @@ class UC2Client(object):
         else:
             self.logger.error("No ESP32 device is connected - check IP or Serial port!")
         
+                
+        if not self.isPyScript: from .updater import updater
+        
         # import libraries depending on API version
-        if self.APIVersion == 1:
-            self.logger.debug("Using API version 1")
-            from .v1.galvo import Galvo
-            from .v1.config import config
-            from .v1.logger import Logger
-            from .v1.ledmatrix import LedMatrix
-            from .v1.motor import Motor
-            from .v1.state import State
-            from .v1.laser import Laser
-            from .v1.wifi import Wifi
-            from .v1.camera import Camera
-            from .v1.analog import Analog
-        elif self.APIVersion == 2:    
-            self.logger.debug("Using API version 2")        
-            from .galvo import Galvo
-            from .config import config
-            from .ledmatrix import LedMatrix
-            from .motor import Motor
-            from .home import Home
-            from .state import State
-            from .laser import Laser
-            from .wifi import Wifi
-            from .camera import Camera
-            from .analog import Analog
-            if not self.isPyScript: from .updater import updater
-            from .modules import Modules
-            from .digitalout import DigitalOut
-
+        self.logger.debug("Using API version 2")        
 
         #FIXME
         #self.set_state(debug=False)
@@ -120,11 +101,17 @@ class UC2Client(object):
         if not self.isPyScript: 
             self.config = config(self)
 
+        # initialize cmdRecorder
+        self.cmdRecorder = cmdRecorder(self)
+        
         # initialize LED matrix
         self.led = LedMatrix(self, NLeds=NLeds)
 
         # initilize motor
         self.motor = Motor(self)
+        
+        # initialize rotator
+        self.rotator = Rotator(self)
         
         # initiliaze homing
         self.home = Home(self)
@@ -151,7 +138,7 @@ class UC2Client(object):
         self.digitalout = DigitalOut(self)
         
         # initialize config
-        if not self.isPyScript: 
+        if False: # not self.isPyScript: 
             self.config = config(self)
             try: self.pinConfig = self.config.loadConfigDevice()
             except: self.pinConfig = None
@@ -163,29 +150,41 @@ class UC2Client(object):
         
         # initialize module controller
         self.modules = Modules(parent=self)
-   
-    def post_json(self, path, payload, getReturn=True, timeout=1):
+    
+    def post_json(self, path, payload, getReturn=True, nResponses=1, timeout=1):
         if self.is_wifi:
             # FIXME: this is not working
             url = f"http://{self.host}:{self.port}{path}"
-            r = requests.post(url, json=payload, headers=self.headers,  timeout=timeout)
-            returnMessage = r.json()
-            returnMessage["success"] = r.status_code==200
+            try:
+                if timeout==0: timeout=.2
+                r = requests.post(url, json=payload, headers=self.headers,  timeout=timeout)
+                returnMessage = r.json()
+                returnMessage["success"] = r.status_code==200
+            except Exception as e:
+                print(e)
+                returnMessage = {}
+                returnMessage["error"] = str(e)
+                returnMessage["success"] = 0
             return returnMessage
         elif self.is_serial or self.isPyScript:
-            return self.serial.post_json(path, payload, getReturn=getReturn, timeout=timeout)
+            if timeout <=0:
+                getReturn = False
+            return self.serial.post_json(path, payload, getReturn=getReturn, nResponses=nResponses)
         else:
             self.logger.error("No ESP32 device is connected - check IP or Serial port!")
             return None
 
-    def get_json(self, path, getReturn=False, timeout=1):
+    def get_json(self, path, getReturn=True, timeout=1):
         if self.is_wifi:
             # FIXME: this is not working
             url = f"http://{self.host}:{self.port}{path}"
             r = requests.get(url, headers=self.headers, timeout=timeout)
             return r.json()
         elif self.is_serial or self.isPyScript:
-            return self.serial.get_json(path)
+            # timeout is not used anymore
+            if timeout <=0:
+                getReturn = False
+            return self.serial.post_json(path, payload=None, getReturn=getReturn, nResponses=1)
             #return self.serial.read_json()
         else:
             self.logger.error("No ESP32 device is connected - check IP or Serial port!")
@@ -194,3 +193,6 @@ class UC2Client(object):
     def setDebugging(self, debug=False):
         self.logger.debug(f"Setting debugging to {debug}")
         self.serial.DEBUG = debug
+        
+    def close(self):
+        self.serial.closeSerial()
--- uc2rest/home.py
+++ uc2rest/home.py
@@ -9,8 +9,9 @@ class Home(object):
         self.direction = 1
         self.speed = 15000
         self.timeout = 20000
+        self.endstoppolarity = 1
         
-    def home_x(self, speed = None, direction = None, endposrelease = None, timeout=None, isBlocking=False):
+    def home_x(self, speed = None, direction = None, endposrelease = None, endstoppolarity=None, timeout=None, isBlocking=False):
         # axis = 1 corresponds to 'X'
         axis = 1
         self.home(axis=axis, 
@@ -18,9 +19,10 @@ class Home(object):
                   speed = speed, 
                   direction = direction, 
                   endposrelease=endposrelease,
+                  endstoppolarity=endstoppolarity,
                   isBlocking=isBlocking)
 
-    def home_y(self, speed = None, direction = None, endposrelease = None, timeout=None, isBlocking=False):
+    def home_y(self, speed = None, direction = None, endposrelease = None, endstoppolarity=None, timeout=None, isBlocking=False):
         # axis = 2 corresponds to 'Y'
         axis = 2
         self.home(axis=axis, 
@@ -28,9 +30,10 @@ class Home(object):
                   speed = speed, 
                   direction = direction, 
                   endposrelease=endposrelease, 
+                  endstoppolarity=endstoppolarity,
                   isBlocking=isBlocking)    
     
-    def home_z(self, speed = None, direction = None, endposrelease = None, timeout=None, isBlocking=False):
+    def home_z(self, speed = None, direction = None, endposrelease = None, endstoppolarity=None, timeout=None, isBlocking=False):
         # axisa = 3 corresponds to 'Z'
         axis = 3
         self.home(axis=axis, 
@@ -38,9 +41,10 @@ class Home(object):
                   speed = speed, 
                   direction = direction, 
                   endposrelease=endposrelease, 
+                  endstoppolarity=endstoppolarity,
                   isBlocking=isBlocking)    
         
-    def home(self, axis=None, timeout=None, speed=None, direction=None, endposrelease=None, isBlocking=False):
+    def home(self, axis=None, timeout=None, speed=None, direction=None, endposrelease=None, endstoppolarity=None, isBlocking=False):
         '''
         axis = 0,1,2,3 or 'A, 'X','Y','Z'
         timeout => when to stop homing (it's a while loop on the MCU)
@@ -58,6 +62,8 @@ class Home(object):
             endposrelease = self.endposrelease
         if timeout is None:
             timeout = self.timeout
+        if endstoppolarity is None:
+            endstoppolarity = self.endstoppolarity
 
         if direction not in [-1,1]:
             direction = 1
@@ -71,36 +77,19 @@ class Home(object):
                 "steppers": [
                 {
                  "stepperid": axis,
-                 "timeout":timeout*1000,
+                 "timeout":timeout,
                  "speed":speed,
                  "direction":direction,
-                 "endposrelease":endposrelease
+                 "endposrelease":endposrelease, 
+                 "endstoppolarity":endstoppolarity
                  }]
             }}
      
-        # send json string
-        r = self._parent.post_json(path, payload, getReturn=True, timeout=timeout)
+        timeout = timeout if isBlocking else 0
+        nResponses = 2 # one for command received, one for home reached
         
-        # wait until job has been done        
-        time0=time.time()
-        if isBlocking and self._parent.serial.is_connected:
-            while True:
-                time.sleep(0.05) # don'T overwhelm the CPU
-                # see if already done
-                try:
-                    rMessage = self._parent.serial.serialdevice.readline().decode() # TODO: Make sure it's compatible with all motors running at the same time
-                except Exception as e:
-                    self._parent.logger.error(e)
-                    rMessage = ""
-                # check if message contains a motor that is done already
-                if rMessage.find('isDone') >-1:
-                    break
-                if time.time()-time0>timeout:
-                    break
+        # if we get a return, we will receive the latest position feedback from the driver  by means of the axis that moves the longest
+        r = self._parent.post_json(path, payload, getReturn=isBlocking, timeout=timeout, nResponses=nResponses)
 
-
-        
-        
-        
         return r
 
