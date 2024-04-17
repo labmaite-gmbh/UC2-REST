924e50f872d032453aa52d339f79176eb42e1491
matias <matiasgstingl@gmail.com>
2024-04-17 20:03:31 +0200

    Added stop and fixed home_az, Including new device config and test experiment config.

--- uc2rest/home.py
+++ uc2rest/home.py
@@ -42,8 +42,67 @@ class Home(object):
                   direction = direction, 
                   endposrelease=endposrelease, 
                   endstoppolarity=endstoppolarity,
-                  isBlocking=isBlocking)    
-        
+                  isBlocking=isBlocking)
+
+
+    def home_az(self, speed = None, direction = None, endposrelease = None, endstoppolarity=None, timeout=None, isBlocking=False):
+        '''
+                axis = 0,1,2,3 or 'A, 'X','Y','Z'
+                timeout => when to stop homing (it's a while loop on the MCU)
+                speed => speed of homing (0...15000)
+                direction => 1,-1 (left/right)
+                endposrelease => how far to move after homing (0...3000)
+                '''
+
+        # default values
+        if speed is None:
+            speed = self.speed
+        if direction is None:
+            direction = self.direction
+        if endposrelease is None:
+            endposrelease = self.endposrelease
+        if timeout is None:
+            timeout = self.timeout
+        if endstoppolarity is None:
+            endstoppolarity = self.endstoppolarity
+
+        if direction not in [-1, 1]:
+            direction = 1
+
+        # construct json string
+        path = "/home_act"
+
+        payload = {
+            "task": path,
+            "home": {
+                "steppers": [
+                    {
+                        "stepperid": 0,
+                        "timeout": timeout,
+                        "speed": speed,
+                        "direction": direction,
+                        "endposrelease": endposrelease,
+                        "endstoppolarity": endstoppolarity
+                    },
+                    {
+                        "stepperid": 3,
+                        "timeout": timeout,
+                        "speed": speed,
+                        "direction": direction,
+                        "endposrelease": endposrelease,
+                        "endstoppolarity": endstoppolarity
+                    }
+                ]
+            }}
+
+        timeout = timeout if isBlocking else 0
+        nResponses = 2  # one for command received, one for home reached
+
+        # if we get a return, we will receive the latest position feedback from the driver  by means of the axis that moves the longest
+        r = self._parent.post_json(path, payload, getReturn=isBlocking, timeout=timeout, nResponses=nResponses)
+
+        return r
+
     def home(self, axis=None, timeout=None, speed=None, direction=None, endposrelease=None, endstoppolarity=None, isBlocking=False):
         '''
         axis = 0,1,2,3 or 'A, 'X','Y','Z'
